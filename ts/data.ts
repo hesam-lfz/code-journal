8b4ffe9a8b432f1d9863dd5b54e763140522fbc0
Hesam Samimi <hesam.samimi.cs@gmail.com>
2025-04-08 03:17:29 +0000

    up

--- ts/data.ts
+++ ts/data.ts
@@ -1,38 +1,6 @@
-interface JournalEntry {
-  title: string;
-  photoUrl: string;
-  notes: string;
-  entryId: number;
-}
-
-interface Data {
-  view: 'entries' | 'entry-form';
-  entries: JournalEntry[];
-  editing: null | JournalEntry;
-  nextEntryId: number;
-}
-
-const dataKey = 'code-journal-data';
-
-const data = readData();
-
-function readData(): Data {
-  let data: Data;
-  const localData = localStorage.getItem(dataKey);
-  if (localData) {
-    data = JSON.parse(localData) as Data;
-  } else {
-    data = {
-      view: 'entry-form',
-      entries: [],
-      editing: null,
-      nextEntryId: 1,
-    };
-  }
-  return data;
-}
-
-function writeData(): void {
-  const dataJSON = JSON.stringify(data);
-  localStorage.setItem(dataKey, dataJSON);
-}
+const data = {
+  view: 'entry-form',
+  entries: [],
+  editing: null,
+  nextEntryId: 1,
+};
